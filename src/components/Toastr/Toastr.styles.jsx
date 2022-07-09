33b09d9b6408bf48e4268ed32d84b2dd04302842
KhanhGN <kgnguyen@myseneca.ca>
2022-07-09 14:36:15 +0700

    Added z-index for toastr to be on top

--- src/components/Toastr/Toastr.styles.jsx
+++ src/components/Toastr/Toastr.styles.jsx
@@ -2,9 +2,9 @@ import styled from '@emotion/styled';
 
 export const ToastrWarpper = styled.div`
 	position: absolute;
-	transition: 1s ease;
-	top: 70px;
-	z-index: 1000;
+	transition: 1s ease-in-out;
+	top: 40px;
+	z-index: 10000;
 	&.in {
 		right: 10px;
 		animation: flyIn 1s ease;
